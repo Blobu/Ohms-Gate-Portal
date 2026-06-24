a069a160257b5ad19ff31eb3484c521df7b5c984
DenisaCozma <d22889526@gmail.com>
2026-06-24 21:59:22 +0300

    delete, insert, search added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { provideNzI18n, en_US } from 'ng-zorro-antd/i18n';
 
 import { routes } from './app.routes';
+
+registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideNzI18n(en_US),
+  ],
 };
